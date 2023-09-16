a74032366f0e5db9c01e4b3202fd0022f7140bd0
shreekant wagh <shreekant.wagh@scania.com>
2023-09-16 12:44:35 +0200

    add button for copy

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 async function fetchData() {
-    const jsonData = [
-        { id: 1, name: "blah.blah@xyz.com" },
-        { id: 2, name: "blah" },
-        { id: 3, name: "blahblah" },
-        { id: 4, name: "+46 XXXXXX" }
+    var jsonData = [
+        
+        { key: 2, value: "Shreekant" },
+        { key: 3, value: "Wagh" },
+
     ];
 
-    const dataTable = document.getElementById('data-table').getElementsByTagName('tbody')[0];
+    // Function to create a "Copy" button
+    function createCopyButton(text) {
+        var button = document.createElement("button");
+        button.textContent = text;
+        button.addEventListener("click", function () {
+            var row = this.parentNode.parentNode; // Get the parent row of the clicked button
+            var sourceCell = row.cells[1];
+            navigator.clipboard.writeText(sourceCell.textContent).then(function () {
+                //alert("Copied toclipboard: " + sourceCell.textContent);
+            }).catch(function (error) {
+                alert("alert");
+            });
+        });
+        return button;
+    }
 
-    jsonData.forEach(item => {
-        const row = dataTable.insertRow();
-        const cell1 = row.insertCell();
-        cell1.textContent = item.name;
-    });
+    // Populate the table with JSON data
+    var table = document.getElementById("data-table").getElementsByTagName('tbody')[0];
+
+    for (var i = 0; i < jsonData.length; i++) {
+        var row = table.insertRow(i);
+        var cellCopy = row.insertCell(0);
+        var cellValue = row.insertCell(1);
+
+        cellCopy.appendChild(createCopyButton("CP"));
+
+        cellValue.innerHTML = jsonData[i].value;
+    }
 }
 fetchData();
